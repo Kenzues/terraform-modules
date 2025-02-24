a15bb0fc7126d0ae26d40644a078419013f7cf37
kenzues <kwaithaka540@gmail.com>
2025-02-24 15:31:42 +0300

    created sucirity groups

--- kens-website-ecs/backend.tf
+++ kens-website-ecs/backend.tf
@@ -1,9 +1,9 @@
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = "waithakas-terraform-remote-backend010"
-    key       = "kens-website-ecs.tfstate"
-    region    = "us-east-1"
-    profile   = "terraform-user"
+    bucket  = "waithakas-terraform-remote-backend010"
+    key     = "kens-website-ecs.tfstate"
+    region  = "us-east-1"
+    profile = "terraform-user"
   }
 }
--- kens-website-ecs/main.tf
+++ kens-website-ecs/main.tf
@@ -6,16 +6,16 @@ provider "aws" {
 
 #create vpc
 module "vpc" {
-  source                        = "../modules/vpc"
-  region                        = var.region
-  project_name                  = var.project_name
-  vpc_cidr                      = var.vpc_cidr
-  public_subnet_az1_cidr        = var.public_subnet_az1_cidr
-  public_subnet_az2_cidr        = var.public_subnet_az2_cidr
-  private_app_subnet_az1_cidr   = var.private_app_subnet_az1_cidr
-  private_app_subnet_az2_cidr   = var.private_app_subnet_az2_cidr
-  private_data_subnet_az1_cidr  = var.private_data_subnet_az1_cidr
-  private_data_subnet_az2_cidr  = var.private_data_subnet_az2_cidr
+  source                       = "../modules/vpc"
+  region                       = var.region
+  project_name                 = var.project_name
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
 
 #create NAT gateway
@@ -30,3 +30,10 @@ module "nat-gateway" {
   private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
   private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
 }
+
+module "security-group" {
+  source = "../modules/security-groups"
+  vpc_id = module.vpc.vpc_id
+
+}
+
